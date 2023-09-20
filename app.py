f3f60a908fdd2bcc7befe8ef1c27a0959298c123
Akash <akashprakashkrishnan@gmail.com>
2023-09-20 16:59:25 +0530

    triggering apprunner

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def predict():
         return render_template('result.html', results= pred, distance=distance)
     
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=7000)
